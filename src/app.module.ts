6707f57a3a9ea8a2dbc94cf500f0822aedce4e90
Artem Kireev <artemoax@gmail.com>
2020-07-03 17:46:23 +0300

    prisma init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}

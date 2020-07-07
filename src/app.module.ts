5c15910821804b984ff13ce8cf10a3aff0c7ab8e
Artem Kireev <artemoax@gmail.com>
2020-07-07 21:01:47 +0300

    configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthModule } from './auth/auth.module';
 import ormconfig from './ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(ormconfig),
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
